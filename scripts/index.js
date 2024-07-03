b8d6d8addf7de55a6371efbd798d2e5e86c262eb
David (Favour) Alagwu <57830927+Alagwudavid@users.noreply.github.com>
2024-07-03 13:39:14 +0100

    update on data-testId

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,3 @@ const currentTimeUTC = document.getElementById("currentTimeUTC");
       const now = new Date();
       const utcTime = new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds());
       currentTimeUTC.innerText = utcTime.toUTCString();
-      currentTimeUTC.setAttribute('data-testid', utcTime);
-
-      console.log(utcTime);
